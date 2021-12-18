1cee0b91c079a87f69f5e3049eafc4492d2aa4fa
davidciliberto1 <davidciliberto@gmail.com>
2021-12-18 11:49:47 -0300

    modified buttons header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,32 @@ import Link from './Link';
 
 const Header = () => {
     return (
-        <div className='ui secondary pointing menu'>
-            <Link href='/' className='item'>Accordion</Link>
-            <Link href='/list' className='item'>Search</Link>
-            <Link href='/dropdown' className='item'>Dropdown</Link>
-            <Link href='/translate' className='item'>Translate</Link>
+        <div className='ui container'>
+            <div class="ui animated fade button" tabindex="0">
+                <div class="visible content">Accordion</div>
+                <div class="hidden content">
+                    <Link href='/' className='item'>Personal Info</Link>
+                </div>
+            </div>
+            <div class="ui animated fade button" tabindex="0">
+                <div class="visible content">Search</div>
+                <div class="hidden content">
+                    <Link href='/list' className='item'>Search</Link>
+                </div>
+            </div>
+            <div class="ui animated fade button" tabindex="0">
+                <div class="visible content">Dropdown</div>
+                <div class="hidden content">
+                    <Link href='/dropdown' className='item'>Dropdown</Link>
+                </div>
+            </div>
+            <div class="ui animated fade button" tabindex="0">
+                <div class="visible content">Translate</div>
+                <div class="hidden content">
+                    <Link href='/translate' className='item'>Translate</Link>
+                </div>
+            </div>
+
         </div>
     );
 };
